6785fee3e6569bdec6fcdb208a0cd3373ff87a60
Jesse Lind <Jesseclind@gmail.com>
2021-12-10 14:01:58 -0500

    Working on login for saving current position in user's workout for refresh retreival

--- src/pages/Workout/Workout.js
+++ src/pages/Workout/Workout.js
@@ -2,25 +2,54 @@ import React, { useState, useEffect } from 'react'
 import { useWorkout } from '../../contexts/WorkoutContext'
 import WorkoutSelection from '../../components/Workout/WorkoutSelection'
 // import ActiveWorkoutContainer from '../../components/Workout/ActiveWorkoutContainer'
+import { e1 } from '../../assets/data/e1'
 
 const Workout = () => {
   const { workoutData, updateWorkout } = useWorkout()
   const [loading, setLoading] = useState(false)
 
+  const [currIdx, setCurrIdx] = useState()
+  const [currSet, setCurrSet] = useState()
+  const [currSetTotal, setCurrSetTotal] = useState()
+
   const { isWorkoutRunning } = workoutData
 
   const startWorkout = exercise => {
-    const data = { isWorkoutRunning: true }
+    const data = {
+      isWorkoutRunning: true,
+      runningWorkout: {
+        remainingWorkout: { currIdx: 0, currSet: 1 },
+        currWorkout: e1.path,
+      },
+    }
     setLoading(true)
     updateWorkout(data).then(() => {
-      console.log(workoutData)
       setLoading(false)
     })
   }
+  const completeSet = () => {}
   const stopWorkout = () => {
-    const data = { isWorkoutRunning: false }
+    const data = {
+      isWorkoutRunning: false,
+      runningWorkout: {
+        remainingWorkout: null,
+        currWorkout: null,
+      },
+    }
     updateWorkout(data)
   }
+
+  useEffect(() => {
+    if (isWorkoutRunning) {
+      const currWorkoutData = workoutData.runningWorkout
+      const currIdx = currWorkoutData.remainingWorkout.currIdx
+      const currSet = currWorkoutData.remainingWorkout.currSet
+      setCurrIdx(currIdx)
+      setCurrSet(currSet)
+      setCurrSetTotal(currWorkoutData.currWorkout[currIdx].sets)
+    }
+  }, [])
+
   return (
     <>
       <div className='page'>
@@ -31,7 +60,12 @@ const Workout = () => {
           <>
             {isWorkoutRunning ? (
               <>
-                bingo
+                {currSet ? (
+                  <div>{`Current Set ${currSet} / ${currSetTotal}`}</div>
+                ) : null}
+                <button className='submit-btn' onClick={completeSet}>
+                  Complete Set
+                </button>
                 <button className='submit-btn' onClick={stopWorkout}>
                   Stop Workout
                 </button>
