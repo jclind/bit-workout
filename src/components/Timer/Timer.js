9abbab993d357520164ed6a1ad7edeb1cc7c6e5e
Jesse Lind <Jesseclind@gmail.com>
2021-12-14 14:02:40 -0500

    Add styles for workout timer component and add skip rest button logic for skipping to the next workout.

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const Timer = ({ timerVal }) => {
-  return <div>{timerVal}</div>
+const Timer = ({ timerVal, skipRestBtn }) => {
+  return (
+    <div className='workout-timer'>
+      <div className='timer-title'>Take a rest, you deserve it.</div>
+      <div className='timer'>{timerVal}</div>
+      <img
+        src='/workouts/workout-rest.png'
+        alt='Adorable sloths cuddling awwww <33333'
+        className='rest-img'
+      />
+      <button className='skip-rest-btn' ref={skipRestBtn}>
+        Skip Rest
+      </button>
+    </div>
+  )
 }
 
 export default Timer
--- src/components/Timer/TimerContainer.js
+++ src/components/Timer/TimerContainer.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { formatTime } from '../../util/formatTime'
 import Timer from './Timer'
+import '../../assets/styles/components/workout/workout-timer.scss'
 
 const TimerContainer = ({ timerStart, restTime, setIsTimer }) => {
   const [timerVal, setTimerVal] = useState()
+
+  const skipRestBtn = useRef()
+
+  const clearTimer = timer => {
+    clearInterval(timer)
+    setIsTimer(false)
+  }
+
+  const skipTimer = (ref, timer) => {
+    const handler = e => {
+      if (timer) {
+        clearTimer(timer)
+      }
+    }
+    ref.current.addEventListener('click', handler)
+
+    return () => {
+      ref.current.removeEventListener('click', handler)
+    }
+  }
 
   useEffect(() => {
     let timer = setInterval(() => {
@@ -17,13 +38,13 @@ const TimerContainer = ({ timerStart, restTime, setIsTimer }) => {
 
       setTimerVal(formatTime(timeLeft))
       if (timeLeft <= 0) {
-        clearInterval(timer)
-        setIsTimer(false)
+        clearTimer(timer)
       }
     }, 100)
+    skipTimer(skipRestBtn, timer)
   }, [])
 
-  return <Timer timerVal={timerVal} />
+  return <Timer timerVal={timerVal} skipRestBtn={skipRestBtn} />
 }
 
 export default TimerContainer
