4f2886a7129931bbbc002c736733a9d49d71873f
Jesse Lind <Jesseclind@gmail.com>
2022-06-03 15:55:47 -0400

    fix: formatTimeToObject funtion now outputs correct minutes

--- src/util/formatTime.js
+++ src/util/formatTime.js
@@ -11,7 +11,7 @@ export const formatTimeToObject = timeMS => {
   let h,
     m,
     s = 0
-  m = Math.floor(timeMS / 60000)
+  m = Math.floor((timeMS % 3600000) / 60000)
   s = ((timeMS % 60000) / 1000).toFixed(0)
   h = Math.floor((timeMS / (1000 * 60 * 60)) % 24)
 
