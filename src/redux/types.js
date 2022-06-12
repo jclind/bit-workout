35c36714d55c863a7f19d89802e331e68eb3f116
Jesse Lind <Jesseclind@gmail.com>
2022-06-12 15:43:39 -0400

    build: setup redux for character data

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,3 +1,4 @@
+// Auth types
 export const SIGN_IN = 'SIGN_IN'
 export const SIGN_OUT = 'SIGN_OUT'
 export const FETCH_USER_ACCOUNT_DATA = 'FETCH_USER_ACCOUNT_DATA'
@@ -6,6 +7,11 @@ export const SET_USER_STATUS_SIGNED_IN = 'SET_USER_STATUS_SIGNED_IN'
 export const SET_USER_STATUS_SIGNED_OUT = 'SET_USER_STATUS_SIGNED_OUT'
 export const SET_USER_ACCOUNT_DATA = 'SET_USER_ACCOUNT_DATA'
 
+// Workout Types
 export const FETCH_WORKOUT_DATA = 'FETCH_WORKOUT_DATA'
 export const SET_WORKOUT_DATA = 'SET_WORKOUT_DATA'
 export const SET_WORKOUT_FINISHED = 'SET_WORKOUT_FINISHED'
+
+// Character Types
+export const INC_COINS = 'INC_COINS'
+export const DEC_COINS = 'DEC_COINS'
