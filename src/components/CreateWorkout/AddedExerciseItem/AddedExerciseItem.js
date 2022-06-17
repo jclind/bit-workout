1f2aa86c55dc844b4631eea49581047d021c046f
Jesse Lind <Jesseclind@gmail.com>
2022-06-17 14:46:31 -0400

    clean: break AddedExerciseItem html into seperate components for cleanliness

--- src/components/CreateWorkout/AddedExerciseItem/AddedExerciseItem.js
+++ src/components/CreateWorkout/AddedExerciseItem/AddedExerciseItem.js
@@ -19,73 +19,81 @@ const AddedExerciseItem = ({
   setSets,
   deleteAddedExercise,
 }) => {
-  return (
-    <div className='added-exercise-item'>
-      <div className='select-exercise-container' ref={exerciseContainer}>
-        <button
-          type='button'
-          className='select-exercise btn'
-          onClick={toggleDropdown}
-        >
-          <div className='selector-text'>
-            {selectedExercise ? (
-              <>
+  const ChooseExerciseBtn = () => {
+    return (
+      <button
+        type='button'
+        className='select-exercise btn'
+        onClick={toggleDropdown}
+      >
+        <div className='selector-text'>
+          {selectedExercise ? (
+            <>
+              <div className='img-container'>
+                <img src={selectedExercise.imageURL} alt='' />
+              </div>
+              <div className='name'>
+                {selectedExercise.name}{' '}
+                <span className='weights'>
+                  {selectedExercise.weights ? '(Weights)' : '(No Weights)'}
+                </span>
+              </div>
+            </>
+          ) : (
+            <div className='text'>Choose An Exercise</div>
+          )}
+
+          <FiChevronDown className='icon' />
+        </div>
+      </button>
+    )
+  }
+
+  const ExerciseDropdown = () => {
+    return (
+      <>
+        <label className='dropdown-cell'>Choose An Exercise</label>
+        <div className='dropdown-cell'>
+          <input
+            className='search-input'
+            placeholder='Find Exercise'
+            value={exerciseSearchVal}
+            onChange={e => setExerciseSearchVal(e.target.value)}
+          />
+        </div>
+        <div className='exercise-cells'>
+          {exerciseList.slice(0, 50).map(ex => {
+            if (
+              exerciseSearchVal &&
+              !ex.name.toLowerCase().includes(exerciseSearchVal.toLowerCase())
+            ) {
+              return null
+            }
+            return (
+              <div
+                className='dropdown-cell exercise-cell'
+                onClick={() => selectExercise(ex)}
+                key={ex.id}
+              >
                 <div className='img-container'>
-                  <img src={selectedExercise.imageURL} alt='' />
+                  <img src={ex.imageURL} alt='' />
                 </div>
                 <div className='name'>
-                  {selectedExercise.name}{' '}
+                  {ex.name}{' '}
                   <span className='weights'>
-                    {selectedExercise.weights ? '(Weights)' : '(No Weights)'}
+                    {ex.weights ? '(Weights)' : '(No Weights)'}
                   </span>
                 </div>
-              </>
-            ) : (
-              <div className='text'>Choose An Exercise</div>
-            )}
-
-            <FiChevronDown className='icon' />
-          </div>
-        </button>
-        <div className={isDropdown ? 'dropdown visible' : 'dropdown'}>
-          <label className='dropdown-cell'>Choose An Exercise</label>
-          <div className='dropdown-cell'>
-            <input
-              className='search-input'
-              placeholder='Find Exercise'
-              value={exerciseSearchVal}
-              onChange={e => setExerciseSearchVal(e.target.value)}
-            />
-          </div>
-          <div className='exercise-cells'>
-            {exerciseList.slice(0, 50).map(ex => {
-              if (
-                exerciseSearchVal &&
-                !ex.name.toLowerCase().includes(exerciseSearchVal.toLowerCase())
-              ) {
-                return null
-              }
-              return (
-                <div
-                  className='dropdown-cell exercise-cell'
-                  onClick={() => selectExercise(ex)}
-                  key={ex.id}
-                >
-                  <div className='img-container'>
-                    <img src={ex.imageURL} alt='' />
-                  </div>
-                  <div className='name'>
-                    {ex.name}{' '}
-                    <span className='weights'>
-                      {ex.weights ? '(Weights)' : '(No Weights)'}
-                    </span>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+              </div>
+            )
+          })}
         </div>
-      </div>
+      </>
+    )
+  }
+
+  const RepSetSelection = () => {
+    return (
       <div className='inputs'>
         <div className='reps-input-container'>
           <div className='label'>Num Reps:</div>
@@ -112,6 +120,18 @@ const AddedExerciseItem = ({
           />
         </div>
       </div>
+    )
+  }
+
+  return (
+    <div className='added-exercise-item'>
+      <div className='select-exercise-container' ref={exerciseContainer}>
+        <ChooseExerciseBtn />
+        <div className={isDropdown ? 'dropdown visible' : 'dropdown'}>
+          <ExerciseDropdown />
+        </div>
+      </div>
+      <RepSetSelection />
       <button
         type='button'
         className='remove-exercise-btn btn'
