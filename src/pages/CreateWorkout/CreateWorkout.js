495b854677d2c3b7f8ad17689f87aadc021cb6c4
Jesse Lind <Jesseclind@gmail.com>
2022-06-17 20:23:56 -0400

    fix: props not passed correctly into SingleWorkoutExercise

--- src/pages/CreateWorkout/CreateWorkout.js
+++ src/pages/CreateWorkout/CreateWorkout.js
@@ -8,78 +8,62 @@ import {
 } from 'react-icons/ai'
 import AddedExerciseItemContainer from '../../components/CreateWorkout/AddedExerciseItem/AddedExerciseItemContainer'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-import { v4 as uuidv4 } from 'uuid'
-import { connect } from 'react-redux'
 import './CreateWorkout.scss'
-import { createWorkout } from '../../redux/actions/workout/workout'
-import { useNavigate } from 'react-router-dom'
 import { TailSpin } from 'react-loader-spinner'
 
-const CreateWorkout = ({ createWorkout }) => {
-  const titleRef = useRef()
-  const [error, setError] = useState('')
-  // Scroll to top of page if there is an error
-  useEffect(() => {
-    if (error && titleRef.current) {
-      titleRef.current.scrollIntoView()
-    }
-  }, [error])
+const ExerciseItem = ({
+  item,
+  idx,
+  changeAddedExerciseData,
+  deleteAddedExercise,
+}) => {
+  return (
+    <Draggable key={item.id} draggableId={`draggable-${idx}`} index={idx}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          className='add-exercise-drag-container'
+        >
+          <div className='handle-icon-container' {...provided.dragHandleProps}>
+            <AiOutlineMenu className='icon handle-icon' />
+          </div>
+          <AddedExerciseItemContainer
+            item={item}
+            className={snapshot.isDraggingOver && 'dragging'}
+            changeAddedExerciseData={changeAddedExerciseData}
+            deleteAddedExercise={deleteAddedExercise}
+          />
+          {provided.placeholder}
+        </div>
+      )}
+    </Draggable>
+  )
+}
 
-  const [loading, setLoading] = useState(false)
-
-  const [workoutName, setWorkoutName] = useState('')
-  const [workoutDescription, setWorkoutDescription] = useState('')
-
-  const [restTimeMS, setRestTimeMS] = useState('')
-  const [restTimeMinutes, setRestTimeMinutes] = useState('')
-  const [restTimeSeconds, setRestTimeSeconds] = useState('')
-
-  // When restTime Minutes or Seconds changes, set totalTestTime to the total time in seconds
-  useEffect(() => {
-    setRestTimeMS((restTimeMinutes * 60 + restTimeSeconds) * 1000)
-    console.log(restTimeMinutes, restTimeSeconds)
-  }, [restTimeMinutes, restTimeSeconds])
-
-  useEffect(() => {
-    console.log(restTimeMS)
-  }, [restTimeMS])
-  const [failedRestTimeMS, setFailedRestTimeMS] = useState('')
-  const [failedRestTimeMinutes, setFailedRestTimeMinutes] = useState('')
-  const [failedRestTimeSeconds, setFailedRestTimeSeconds] = useState('')
-  // When failedRestTime Minutes or Seconds changes, set totalFailedRestTime to the total time in seconds
-  useEffect(() => {
-    setFailedRestTimeMS(
-      (failedRestTimeMinutes * 60 + failedRestTimeSeconds) * 1000
-    )
-  }, [failedRestTimeMinutes, failedRestTimeSeconds])
-
-  const [addedExercises, setAddedExercises] = useState([])
-
-  const navigate = useNavigate()
-
-  const changeAddedExerciseData = (data, id) => {
-    const currAddedExercisesData = [...addedExercises]
-
-    const addedExerciseIdx = addedExercises.findIndex(ex => ex.id === id)
-
-    currAddedExercisesData[addedExerciseIdx] = {
-      ...currAddedExercisesData[addedExerciseIdx],
-      ...data,
-    }
-
-    setAddedExercises(currAddedExercisesData)
-  }
-  const deleteAddedExercise = id => {
-    setAddedExercises(addedExercises.filter(ex => ex.id !== id))
-  }
-
-  const addExercise = () => {
-    setAddedExercises([
-      ...addedExercises,
-      { exercise: null, reps: null, sets: null, id: uuidv4() },
-    ])
-  }
-
+const CreateWorkout = ({
+  titleRef,
+  handleCreateWorkoutSubmit,
+  workoutName,
+  setWorkoutName,
+  workoutDescription,
+  setWorkoutDescription,
+  restTimeMinutes,
+  setRestTimeMinutes,
+  restTimeSeconds,
+  setRestTimeSeconds,
+  failedRestTimeMinutes,
+  setFailedRestTimeMinutes,
+  failedRestTimeSeconds,
+  setFailedRestTimeSeconds,
+  addedExercises,
+  setAddedExercises,
+  changeAddedExerciseData,
+  deleteAddedExercise,
+  addExercise,
+  error,
+  loading,
+}) => {
   // Help reordering addedExercise list dragged items
   const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list)
@@ -102,67 +86,6 @@ const CreateWorkout = ({ createWorkout }) => {
     )
 
     setAddedExercises(items)
-  }
-
-  const clearForm = () => {
-    setWorkoutName('')
-    setWorkoutDescription('')
-    setRestTimeMinutes('')
-    setRestTimeSeconds('')
-    setFailedRestTimeMinutes('')
-    setFailedRestTimeSeconds('')
-    setAddedExercises([])
-  }
-
-  const handleCreateWorkoutSubmit = e => {
-    e.preventDefault()
-    setError('')
-    setLoading(true)
-
-    if (!workoutName) return setError('Please Enter Workout Name')
-    if (addedExercises.length <= 0)
-      return setError('Please Add Exercise To Workout Path')
-
-    let allExercisesValid = true
-    // Check that every addedExercise has all fields selected
-    addedExercises.forEach(ex => {
-      if (
-        !ex.exercise ||
-        !ex.reps ||
-        ex.reps === 0 ||
-        !ex.sets ||
-        ex.sets === 0
-      )
-        return (allExercisesValid = false)
-    })
-    if (!allExercisesValid)
-      return setError('Please Fill All Exercise Path Fields In Workout Path')
-
-    const newWorkout = {
-      id: uuidv4(),
-      name: workoutName,
-      restTime: restTimeMS > 0 ? restTimeMS : 90000,
-      failSetRestTime: failedRestTimeMS > 0 ? failedRestTimeMS : 300000,
-      lastSetFailed: false,
-      path: addedExercises.map(ex => {
-        return {
-          id: uuidv4(),
-          exerciseID: ex.exercise.id,
-          reps: ex.reps,
-          sets: ex.sets,
-        }
-      }),
-    }
-
-    createWorkout(newWorkout)
-      .then(() => {
-        clearForm()
-        navigate('/')
-      })
-      .catch(err => {
-        setError(`ERROR: ${err.code}`)
-      })
-    setLoading(false)
   }
 
   return (
@@ -270,37 +193,12 @@ const CreateWorkout = ({ createWorkout }) => {
                   >
                     {addedExercises.map((item, idx) => {
                       return (
-                        <Draggable
-                          key={item.id}
-                          draggableId={`draggable-${idx}`}
-                          index={idx}
-                        >
-                          {(provided, snapshot) => (
-                            <div
-                              ref={provided.innerRef}
-                              {...provided.draggableProps}
-                              className='add-exercise-drag-container'
-                            >
-                              <div
-                                className='handle-icon-container'
-                                {...provided.dragHandleProps}
-                              >
-                                <AiOutlineMenu className='icon handle-icon' />
-                              </div>
-                              <AddedExerciseItemContainer
-                                item={item}
-                                className={
-                                  snapshot.isDraggingOver && 'dragging'
-                                }
-                                changeAddedExerciseData={
-                                  changeAddedExerciseData
-                                }
-                                deleteAddedExercise={deleteAddedExercise}
-                              />
-                              {provided.placeholder}
-                            </div>
-                          )}
-                        </Draggable>
+                        <ExerciseItem
+                          item={item}
+                          idx={idx}
+                          changeAddedExerciseData={changeAddedExerciseData}
+                          deleteAddedExercise={deleteAddedExercise}
+                        />
                       )
                     })}
                     {provided.placeholder}
@@ -338,10 +236,4 @@ const CreateWorkout = ({ createWorkout }) => {
   )
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    createWorkout: data => dispatch(createWorkout(data)),
-  }
-}
-
-export default connect(null, mapDispatchToProps)(CreateWorkout)
+export default CreateWorkout
