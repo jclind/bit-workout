629b9087db2f5f15429f4ea116aba211082fc664
Jesse Lind <Jesseclind@gmail.com>
2022-06-27 15:52:14 -0400

    build: add data and styling to PastWorkoutsItem collapseable component

--- src/components/PastWorkoutsItem/PastWorkoutsItem.js
+++ src/components/PastWorkoutsItem/PastWorkoutsItem.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import { formatAMPM, formatDate } from '../../util/formatDate'
 import { formatTimeToObject } from '../../util/formatTime'
-import { AiOutlineCalendar, AiOutlineStar } from 'react-icons/ai'
+import {
+  AiOutlineCalendar,
+  AiOutlineStar,
+  AiOutlineRight,
+  AiOutlineDown,
+} from 'react-icons/ai'
 import { RiCopperCoinLine } from 'react-icons/ri'
 import Skeleton from 'react-loading-skeleton'
 
 import './PastWorkoutsItem.scss'
 import WorkoutTime from '../WorkoutTime/WorkoutTime'
+import { exerciseList } from '../../assets/data/exerciseList'
 
 const PastWorkoutsItem = ({ workout }) => {
   const [isCollapsed, setIsCollapsed] = useState(false)
@@ -17,6 +23,7 @@ const PastWorkoutsItem = ({ workout }) => {
   const startTime = formatAMPM(workout.workoutStartTime)
   const workoutFinishTime =
     Number(workout.workoutStartTime) + Number(workout.totalWorkoutTime)
+  console.log(workout)
   const finishTime = formatAMPM(workoutFinishTime)
   const imageURL = workout.path[0].imageURL
   const coins = workout.coinsEarned
@@ -44,22 +51,87 @@ const PastWorkoutsItem = ({ workout }) => {
           {/* <AiOutlineCalendar className='icon' /> */}
           {date ? date : <Skeleton />}
         </div>
+        <div className='collapse-indicator'>
+          {isCollapsed ? (
+            <AiOutlineRight className='icon ' />
+          ) : (
+            <AiOutlineDown className='icon' />
+          )}
+        </div>
       </div>
+      {/* <div className='collapse-indicator'>
+        {isCollapsed ? (
+          <AiOutlineCaretUp className='icon' />
+        ) : (
+          <AiOutlineCaretDown className='icon' />
+        )}
+      </div> */}
       <div className={isCollapsed ? 'collapse' : 'collapse show'}>
-        <h1>sus</h1>
-        <h1>sus</h1>
-        <h1>sus</h1>
-        <h1>sus</h1>
+        <div className='workout-data'>
+          <div className='start-finish-time item'>
+            <div className='label'>Timespan:</div>
+            <div className='data'>
+              {startTime} - {finishTime}
+            </div>
+          </div>
+          <div className='coins item'>
+            <div className='label'>Coins Earned:</div>
+            <div className='data coins'>
+              + <AiOutlineStar className='icon' />
+              {coins}
+            </div>
+          </div>
+          <div className='exp item'>
+            <div className='label'>Experience Earned:</div>
+            <div className='data exp'>
+              + <RiCopperCoinLine className='icon' />
+              {exp}
+            </div>
+          </div>
+          <div className='workout-path item'>
+            <div className='label'>Exercises:</div>
+            <div className='data'>
+              {workout.path.map(currExercise => {
+                const exerciseData = exerciseList.find(
+                  obj => obj.id === currExercise.exerciseID
+                )
+
+                const name = exerciseData.name
+                const imageURL = exerciseData.imageURL
+                const weight = currExercise.weight
+                const reps = currExercise.reps
+                const sets = currExercise.sets
+
+                return (
+                  <div className='workout-path-exercise'>
+                    <div className='image'>
+                      <img src={imageURL} alt={name} />
+                    </div>
+                    <div className='exercise-data'>
+                      <div className='name'>{name}</div>
+                      <div className='reps-sets'>
+                        <div className='sets'>
+                          Sets: <span>{sets}</span>
+                        </div>
+                        <div className='reps'>
+                          Reps: <span>{reps}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* <div className='character-data'>
         <div className='coins'>
-          + <AiOutlineStar className='icon' />
-          {coins}
+          
         </div>
         <div className='exp'>
-          + <RiCopperCoinLine className='icon' />
-          {exp}
+          
         </div>
       </div> */}
     </div>
