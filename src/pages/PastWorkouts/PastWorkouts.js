355bd67b59f68fef751dba232ff28243ba6580ab
Jesse Lind <Jesseclind@gmail.com>
2022-06-28 20:44:08 -0400

    build: add sort selection and functionality

--- src/pages/PastWorkouts/PastWorkouts.js
+++ src/pages/PastWorkouts/PastWorkouts.js
@@ -4,8 +4,10 @@ import { connect } from 'react-redux'
 import PastWorkoutsItem from '../../components/PastWorkoutsItem/PastWorkoutsItem'
 import { queryPastWorkoutData } from '../../redux/actions/workout/workout'
 import noDataImage from '../../assets/images/no-past-workout-data.svg'
+import { BsSortDown } from 'react-icons/bs'
 import './PastWorkouts.scss'
 import { Link } from 'react-router-dom'
+import Select from 'react-select'
 
 const NoPastWorkoutData = () => {
   return (
@@ -25,36 +27,107 @@ const NoPastWorkoutData = () => {
   )
 }
 
+const sortSelectStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    minHeight: '40px',
+    background: 'none',
+    height: '40px',
+    margin: '0 auto',
+    boxShadow: state.isFocused ? null : null,
+    outline: 'none',
+    border: state.isFocused ? '1px solid #548ca8' : '1px solid #bebebe',
+    ':hover': {
+      border: '1px solid #548ca8',
+    },
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: 'white',
+    fontWeight: '500',
+    paddingBottom: '3px',
+    background: 'none',
+  }),
+
+  valueContainer: (provided, state) => ({
+    ...provided,
+    height: '40px',
+    padding: '0 6px',
+  }),
+
+  input: (provided, state) => ({
+    ...provided,
+    color: '#fff',
+    margin: '0px',
+  }),
+  indicatorSeparator: state => ({
+    display: 'none',
+  }),
+  indicatorsContainer: (provided, state) => ({
+    ...provided,
+    height: '40px',
+  }),
+  placeholder: (provided, state) => ({
+    ...provided,
+    fontWeight: '500',
+    color: '#bebebe',
+  }),
+  menu: (provided, state) => ({
+    ...provided,
+    background: '#334257',
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    background: state.isSelected
+      ? '#548ca8'
+      : state.isFocused
+      ? '#476072'
+      : 'none',
+    ':active': {
+      background: '#476072',
+    },
+  }),
+}
+
 const PastWorkouts = ({ queryPastWorkoutData }) => {
+  const limit = 10
+
   const [pastWorkoutData, setPastWorkoutData] = useState(null)
-  const [order, setOrder] = useState('workoutStartTime')
-  const [limit, setLimit] = useState(10)
+  const [order, setOrder] = useState({
+    sort: 'workoutStartTime',
+    descending: true,
+  })
   const [latestDoc, setLatestDoc] = useState(null)
   const [isPaginationLoading, setIsPaginationLoading] = useState(false)
   const [isMoreData, setIsMoreData] = useState(true)
 
   useEffect(() => {
-    queryPastWorkoutData(order, limit, latestDoc).then(res => {
-      if (!res.data || res.data.length === 0) {
-        setPastWorkoutData([])
-        return setIsMoreData(false)
+    console.log(order)
+    queryPastWorkoutData(order.sort, limit, null, order.descending).then(
+      res => {
+        if (!res.data || res.data.length === 0) {
+          setPastWorkoutData([])
+          return setIsMoreData(false)
+        }
+        setLatestDoc(res.latestDoc) // Set latestDoc to last element in data
+        setPastWorkoutData(res.data)
       }
-      setLatestDoc(res.latestDoc) // Set latestDoc to last element in data
-      setPastWorkoutData(res.data)
-    })
-  }, [])
+    )
+  }, [order])
 
   const getMoreWorkoutData = () => {
-    queryPastWorkoutData(order, limit, latestDoc).then(res => {
-      if (!res.data || res.data.length === 0) {
-        setIsMoreData(false)
-      } else {
-        const newWorkoutData = [...pastWorkoutData, ...res.data]
-        setLatestDoc(res.latestDoc) // Set latestDoc to last element in new data
-        setPastWorkoutData(newWorkoutData)
+    queryPastWorkoutData(order.sort, limit, latestDoc, order.descending).then(
+      res => {
+        if (!res.data || res.data.length === 0) {
+          setIsMoreData(false)
+        } else {
+          const newWorkoutData = [...pastWorkoutData, ...res.data]
+          setLatestDoc(res.latestDoc) // Set latestDoc to last element in new data
+          setPastWorkoutData(newWorkoutData)
+        }
+        setIsPaginationLoading(false)
       }
-      setIsPaginationLoading(false)
-    })
+    )
   }
 
   const listInnerRef = useRef()
@@ -74,6 +147,41 @@ const PastWorkouts = ({ queryPastWorkoutData }) => {
 
   const isDataLoading = isMoreData && !pastWorkoutData
   const isNoData = !isDataLoading && pastWorkoutData.length === 0
+  console.log(pastWorkoutData)
+
+  const sortOptions = [
+    { value: 'new', label: 'Newest' },
+    { value: 'old', label: 'Oldest' },
+    { value: 'longest', label: 'Longest' },
+    { value: 'shortest', label: 'Shortest' },
+  ]
+
+  const handleSelectChange = e => {
+    switch (e.value) {
+      case 'new':
+        return setOrder({
+          sort: 'workoutStartTime',
+          descending: true,
+        })
+      case 'old':
+        return setOrder({
+          sort: 'workoutStartTime',
+          descending: false,
+        })
+      case 'longest':
+        return setOrder({
+          sort: 'totalWorkoutTime',
+          descending: true,
+        })
+      case 'shortest':
+        return setOrder({
+          sort: 'totalWorkoutTime',
+          descending: false,
+        })
+      default:
+        return
+    }
+  }
 
   return (
     <div
@@ -82,6 +190,21 @@ const PastWorkouts = ({ queryPastWorkoutData }) => {
       ref={listInnerRef}
     >
       {!isNoData && <div className='settings-title'>Past Workouts</div>}
+      {!isNoData && (
+        <div className='sort-options'>
+          <Select
+            options={sortOptions}
+            styles={sortSelectStyles}
+            className='select'
+            placeholder={
+              <div className='select-placeholder'>
+                <BsSortDown /> Sort:
+              </div>
+            }
+            onChange={handleSelectChange}
+          />
+        </div>
+      )}
       <div className='past-workouts-container'>
         {isDataLoading ? (
           <>
@@ -106,8 +229,8 @@ const PastWorkouts = ({ queryPastWorkoutData }) => {
 
 const mapPropsToDispatch = dispatch => {
   return {
-    queryPastWorkoutData: (order, limit, pageNum) =>
-      dispatch(queryPastWorkoutData(order, limit, pageNum)),
+    queryPastWorkoutData: (order, limit, pageNum, descending) =>
+      dispatch(queryPastWorkoutData(order, limit, pageNum, descending)),
   }
 }
 
