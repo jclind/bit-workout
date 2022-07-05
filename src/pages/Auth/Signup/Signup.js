9441213286727707e8aa660e4631db6107302a72
Jesse Lind <Jesseclind@gmail.com>
2022-07-05 01:23:59 -0400

    clean: remove unnecessary comments and code

--- src/pages/Auth/Signup/Signup.js
+++ src/pages/Auth/Signup/Signup.js
@@ -1,96 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useLocation, useOutlet, useNavigate, Navigate } from 'react-router-dom'
-import signupBackground from '../../../assets/images/signup-background.png'
+import React from 'react'
+import { useLocation, useOutlet, Navigate } from 'react-router-dom'
 import './Signup.scss'
 import { connect } from 'react-redux'
-import { signup } from '../../../redux/actions/auth/authStatus'
-import { checkUsernameAvailability } from '../../../redux/actions/auth/authStatus'
 
 export const SignupContext = React.createContext({})
 
-const Signup = ({ signup, isSignedIn }) => {
-  // const [usernameVal, setUsernameVal] = useState('')
-  // const [usernameAvailable, setUsernameAvailable] = useState(null)
-  // const [fullNameVal, setFullNameVal] = useState('')
-  // const [emailVal, setEmailVal] = useState('')
-  // const [passwordVal, setPasswordVal] = useState('')
-
-  // const [genderVal, setGenderVal] = useState('female')
-  // const [birthdayVal, setBirthdayVal] = useState('')
-  // const [heightVal, setHeightVal] = useState({ feet: '', inches: '' })
-  // const [weightVal, setWeightVal] = useState('')
-
-  // useEffect(() => {
-  //   if (usernameVal.trim().length >= 3) {
-  //     checkUsernameAvailability(usernameVal).then(isAvailable => {
-  //       setUsernameAvailable(isAvailable)
-  //     })
-  //   } else {
-  //     setUsernameAvailable(null)
-  //   }
-  // }, [usernameVal])
-
-  // const navigate = useNavigate()
-
-  // async function handleSignup() {
-  //   const payload = {
-  //     usernameVal,
-  //     fullNameVal,
-  //     genderVal,
-  //     birthdayVal,
-  //     heightVal,
-  //     weightVal,
-  //     emailVal,
-  //   }
-  //   try {
-  //     await signup(emailVal, passwordVal, payload)
-  //     navigate('/')
-  //   } catch (error) {
-  //     console.log(error)
-  //     // !ERROR
-  //   }
-  // }
-
+const Signup = ({ isSignedIn }) => {
   const location = useLocation()
-  const monthRef = useRef()
 
   const outlet = useOutlet()
-
-  if (location.pathname === '/signup') {
-    return <Navigate to='/signup/gender' />
-  }
-  // // If the fields from /signup/account-info aren't all filled out
-  // // then redirect back to that page
-  // const signupFieldsAreFilled =
-  //   usernameVal !== null &&
-  //   fullNameVal !== null &&
-  //   emailVal !== null &&
-  //   passwordVal !== null
 
   if (isSignedIn) {
     return <Navigate to='/' />
   }
-
-  // const value = {
-  //   usernameVal,
-  //   setUsernameVal,
-  //   fullNameVal,
-  //   setFullNameVal,
-  //   emailVal,
-  //   setEmailVal,
-  //   passwordVal,
-  //   setPasswordVal,
-  //   handleSignup,
-  //   genderVal,
-  //   setGenderVal,
-  //   birthdayVal,
-  //   setBirthdayVal,
-  //   heightVal,
-  //   setHeightVal,
-  //   weightVal,
-  //   setWeightVal,
-  //   usernameAvailable,
-  // }
+  if (location.pathname === '/signup') {
+    return <Navigate to='/signup/gender' />
+  }
 
   const saveSignupData = (key, value) => {
     const currSignupData = JSON.parse(localStorage.getItem('signup')) || {}
@@ -100,18 +25,11 @@ const Signup = ({ signup, isSignedIn }) => {
   }
 
   const value = {
-    monthRef,
     saveSignupData,
   }
 
   return (
     <SignupContext.Provider value={value} className='signup-container'>
-      {/* <img
-        src={signupBackground}
-        alt='signup background'
-        className='signup-background'
-      />
-      <h1 className='signup-title'>Sign Up</h1> */}
       {outlet}
     </SignupContext.Provider>
   )
@@ -126,11 +44,5 @@ const mapStateToProps = state => {
     isSignedIn: isSignedIn,
   }
 }
-const mapDispatchToProps = dispatch => {
-  return {
-    signup: (email, password, userData) =>
-      dispatch(signup(email, password, userData)),
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signup)
+export default connect(mapStateToProps)(Signup)
