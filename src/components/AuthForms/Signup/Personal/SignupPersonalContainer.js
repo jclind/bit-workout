720cba4d4d5152c99d94c4705cdb6bfaaf23f5a9
Jesse Lind <Jesseclind@gmail.com>
2022-07-07 16:30:24 -0400

    clean: remove old signup pages/routes

--- src/components/AuthForms/Signup/Personal/SignupPersonalContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SignupPersonal from './SignupPersonal'
-
-const SignupPersonalContainer = () => {
-  return <SignupPersonal />
-}
-
-export default SignupPersonalContainer
