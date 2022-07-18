9b3df8cb9371c359407f2b9f5a1dd6661b1be3e0
Jesse Lind <Jesseclind@gmail.com>
2022-07-18 11:14:28 -0400

    build: begin adding workout type selection

---
+++ src/components/CreateWorkout/ExerciseTypeDropdown/ExerciseTypeDropdown.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Select from 'react-select'
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+]
+
+const ExerciseTypeDropdown = () => {
+  return <div></div>
+}
+
+export default ExerciseTypeDropdown
