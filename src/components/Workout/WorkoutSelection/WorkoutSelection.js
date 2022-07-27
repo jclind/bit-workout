f8e71ab5cb882c498ae289d7811591b49a557b89
Jesse Lind <Jesseclind@gmail.com>
2022-07-27 14:56:24 -0400

    build: begin workout selection page refactor

--- src/components/Workout/WorkoutSelection/WorkoutSelection.js
+++ src/components/Workout/WorkoutSelection/WorkoutSelection.js
@@ -1,28 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import SingleWorkout from '../SingleWorkout/SingleWorkout'
 import { AiOutlineSearch, AiOutlinePlusCircle } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import {
+  Link,
+  Navigate,
+  useLocation,
+  useNavigate,
+  useOutlet,
+} from 'react-router-dom'
 import { connect } from 'react-redux'
 import './WorkoutSelection.scss'
 import { getWorkouts } from '../../../redux/actions/workout/workout'
 
 const WorkoutSelection = ({ getWorkouts }) => {
+  const location = useLocation()
+  const navigate = useNavigate()
+  const outlet = useOutlet()
+
   const [workoutSearchVal, setWorkoutSearchVal] = useState('')
+  const [selectedList, setSelectedList] = useState('trending')
+  useEffect(() => {
+    navigate(`/workout/${selectedList}-workouts`)
+  }, [selectedList])
 
   const [workouts, setWorkouts] = useState(null)
-
-  useEffect(() => {
-    getWorkouts('').then(res => {
-      setWorkouts(res)
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   return (
     <div className='workout-selection'>
       <div className='settings-title'>Workout Selection</div>
 
-      <div className='search-workouts-container'>
+      <div className='selector'>
+        <button
+          className={
+            selectedList === 'trending'
+              ? 'selection-btn selected'
+              : 'selection-btn'
+          }
+          onClick={() => setSelectedList('trending')}
+        >
+          Trending
+        </button>
+        <button
+          className={
+            selectedList === 'user' ? 'selection-btn selected' : 'selection-btn'
+          }
+          onClick={() => setSelectedList('user')}
+        >
+          Your Workouts
+        </button>
+      </div>
+      {outlet}
+      {/* <div className='search-workouts-container'>
         <AiOutlineSearch className='icon' />
         <input
           type='text'
@@ -31,8 +59,8 @@ const WorkoutSelection = ({ getWorkouts }) => {
           value={workoutSearchVal}
           onChange={e => setWorkoutSearchVal(e.target.value)}
         />
-      </div>
-      {workouts &&
+      </div> */}
+      {/* {workouts &&
         workouts.slice(0, 8).map(workout => {
           const workoutMatchesSearchVal = workout.name
             .toLowerCase()
@@ -45,7 +73,7 @@ const WorkoutSelection = ({ getWorkouts }) => {
               <SingleWorkout exercise={workout} />
             </React.Fragment>
           )
-        })}
+        })} */}
       <Link to='/create-workout' className='create-workout-link'>
         <button>
           <AiOutlinePlusCircle className='icon' />
