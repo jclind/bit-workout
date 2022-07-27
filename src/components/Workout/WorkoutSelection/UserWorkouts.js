83c8947762c3fc32ac4d2e1c969c003bba1c5477
Jesse Lind <Jesseclind@gmail.com>
2022-07-27 15:25:23 -0400

    build: add getUserWorkouts function

--- src/components/Workout/WorkoutSelection/UserWorkouts.js
+++ src/components/Workout/WorkoutSelection/UserWorkouts.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { getUserWorkouts } from '../../../redux/actions/workout/workout'
 
-const UserWorkouts = () => {
+const UserWorkouts = ({ getUserWorkouts }) => {
+  useEffect(() => {
+    getUserWorkouts().then(res => {
+      console.log(res)
+    })
+  }, [])
+
   return <div>UserWorkouts</div>
 }
 
-export default UserWorkouts
+const mapDispatchToProps = dispatch => {
+  return {
+    getUserWorkouts: (query, order, limit) =>
+      dispatch(getUserWorkouts(query, order, limit)),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(UserWorkouts)
