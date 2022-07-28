558d271a1227201cb5e8b3d8da2e4a80a1687a95
Jesse Lind <Jesseclind@gmail.com>
2022-07-28 01:24:13 -0400

    build: update estimateTime function to get number of sets per exercise

--- src/components/Workout/WorkoutSelection/UserWorkouts.js
+++ src/components/Workout/WorkoutSelection/UserWorkouts.js
@@ -1,15 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { getUserWorkouts } from '../../../redux/actions/workout/workout'
+import SingleWorkout from '../SingleWorkout/SingleWorkout'
 
 const UserWorkouts = ({ getUserWorkouts }) => {
+  const [workouts, setWorkouts] = useState([])
+
   useEffect(() => {
     getUserWorkouts().then(res => {
-      console.log(res)
+      setWorkouts(res)
     })
   }, [])
 
-  return <div>UserWorkouts</div>
+  return (
+    <div className='user-workout workout-list'>
+      {workouts.length > 0 &&
+        workouts.map(workout => {
+          return (
+            <React.Fragment key={workout.id}>
+              <SingleWorkout workout={workout} />
+            </React.Fragment>
+          )
+        })}
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => {
