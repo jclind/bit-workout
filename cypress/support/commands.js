7fc6d91c9208c088b5b0a1a6547a737c8401153e
Jesse Lind <Jesseclind@gmail.com>
2022-08-05 16:18:26 -0400

    test: add straight set 1 exercise user pathing test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-wait-until'
+
+Cypress.Commands.add('changeExerciseWeightThroughPlatesModal', newWeight => {
+  // Click plate weights modal
+  cy.get('.active-workout .exercise-weight').click()
+  cy.get('.plates-modal').should('be.visible')
+  cy.get('.plates-modal .total-weight').should('be.visible')
+
+  // Click total weight/change weight button and change weight to
+  cy.get('.plates-modal .total-weight').click()
+  cy.get('.change-weight-modal').should('be.visible')
+  cy.get('.change-weight-modal input').type(newWeight)
+  cy.get('.change-weight-modal .save-btn').click()
+
+  cy.get('.plates-modal').should('be.visible')
+  cy.get('.plates-modal .total-weight')
+    .contains(`${newWeight} lbs`)
+    .should('be.visible')
+  cy.get('.overlay').click(15, 15) // Close plates modal by overlay click
+})
+Cypress.Commands.add('completeSetAndSkipRest', () => {
+  cy.get('.active-workout button.submit-btn').click()
+  cy.get('.workout-timer button.skip-rest-btn').click()
+})
