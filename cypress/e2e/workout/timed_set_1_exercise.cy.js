2e9063122a4a469d1cdeea14e609b6937056e7f4
Jesse Lind <Jesseclind@gmail.com>
2022-08-10 13:34:30 -0400

    test: finish timed_set_1_exercise testing

--- cypress/e2e/workout/timed_set_1_exercise.cy.js
+++ cypress/e2e/workout/timed_set_1_exercise.cy.js
@@ -1,6 +1,24 @@
 describe('Workout Tests', () => {
-  Cypress.Commands.add('completeTimedSet', skipTimer => {
+  Cypress.Commands.add('completeTimedSet', (skipRest, testRepFunctionality) => {
     cy.contains('Start Timer').click()
+    cy.contains('Skip Timer').click()
+
+    // Test dec, inc and typing rep number
+    if (testRepFunctionality) {
+      cy.get('.rep-input-modal button.decrement').click()
+      cy.get('.rep-input-modal input.rep-input').should('have.value', 4)
+      cy.get('.rep-input-modal button.increment').click()
+      cy.get('.rep-input-modal input.rep-input').should('have.value', 5)
+
+      cy.get('.rep-input-modal input.rep-input').type(10)
+      cy.get('.rep-input-modal input.rep-input').should('have.value', 10)
+    }
+
+    cy.get('.rep-input-modal button.confirm-btn').click()
+
+    if (skipRest) {
+      cy.get('button.skip-rest-btn').click()
+    }
   })
 
   before(() => {
@@ -25,14 +43,13 @@ describe('Workout Tests', () => {
 
     // cy.changeExerciseWeightThroughPlatesModal(80)
     // // Run through workout
+    cy.completeTimedSet(true, true)
     cy.completeTimedSet(true)
-    // cy.completeSetAndSkipRest()
-    // cy.completeSetAndSkipRest()
-    // cy.completeSetAndSkipRest()
-    // cy.completeSetAndSkipRest()
-    // cy.get('.active-workout button.submit-btn').click()
+    cy.completeTimedSet(true)
+    cy.completeTimedSet(true)
+    cy.completeTimedSet()
 
-    // cy.get('.workout-complete.page').should('be.visible')
-    // cy.get('button.back-home-btn').click()
+    cy.get('.workout-complete.page').should('be.visible')
+    cy.get('button.back-home-btn').click()
   })
 })
