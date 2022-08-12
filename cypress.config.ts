aa50991c36b125f91a7e6b643c2116033162e036
Jesse Lind <Jesseclind@gmail.com>
2022-08-12 15:25:02 -0400

    build: add projectId to cypress.config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ import { plugin as cypressFirebasePlugin } from 'cypress-firebase'
 const cypressConfig = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    projectId: 'workout-app-30261',
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       cypressFirebasePlugin(on, config, admin)
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,39 +1,39 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+// // ***********************************************************
+// // This example support/e2e.js is processed and
+// // loaded automatically before your test files.
+// //
+// // This is a great place to put global configuration and
+// // behavior that modifies Cypress.
+// //
+// // You can change the location of this file or turn off
+// // automatically serving support files with the
+// // 'supportFile' configuration option.
+// //
+// // You can read more here:
+// // https://on.cypress.io/configuration
+// // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
-import 'firebase/compat/database'
-import 'firebase/compat/firestore'
-import { attachCustomCommands } from 'cypress-firebase'
+// // Import commands.js using ES2015 syntax:
+// import './commands'
+// import firebase from 'firebase/compat/app'
+// import 'firebase/compat/auth'
+// import 'firebase/compat/database'
+// import 'firebase/compat/firestore'
+// import { attachCustomCommands } from 'cypress-firebase'
 
-const fbConfig = {
-  apiKey: 'AIzaSyBd6q8SnjOSvT6ZB7ANUm68s5OcrifcQy4',
-  authDomain: 'workout-app-30261.firebaseapp.com',
-  projectId: 'workout-app-30261',
-  storageBucket: 'workout-app-30261.appspot.com',
-  messagingSenderId: '656690318764',
-  appId: '1:656690318764:web:848b6e82f5ec93f348fe09',
-  measurementId: 'G-ZEE8NWY6J2',
-}
+// const fbConfig = {
+//   apiKey: 'AIzaSyBd6q8SnjOSvT6ZB7ANUm68s5OcrifcQy4',
+//   authDomain: 'workout-app-30261.firebaseapp.com',
+//   // projectId: 'workout-app-30261',
+//   storageBucket: 'workout-app-30261.appspot.com',
+//   messagingSenderId: '656690318764',
+//   appId: '1:656690318764:web:848b6e82f5ec93f348fe09',
+//   measurementId: 'G-ZEE8NWY6J2',
+// }
 
-firebase.initializeApp(fbConfig)
+// firebase.initializeApp(fbConfig)
 
-attachCustomCommands({ Cypress, cy, firebase })
+// attachCustomCommands({ Cypress, cy, firebase })
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+// // Alternatively you can use CommonJS syntax:
+// // require('./commands')
