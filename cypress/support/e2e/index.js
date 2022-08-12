cc12e41b518bfceb735adc31e4d157adba3d1693
Jesse Lind <Jesseclind@gmail.com>
2022-08-12 16:33:20 -0400

    test: fix import error

--- cypress/support/e2e/index.js
+++ cypress/support/e2e/index.js
@@ -14,7 +14,7 @@
 // // ***********************************************************
 
 // // Import commands.js using ES2015 syntax:
-import './commands'
+import '../commands'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/database'
