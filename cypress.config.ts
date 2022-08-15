b1fdf256faf755ecc62f2170e713ec71d9fc4add
Jesse Lind <Jesseclind@gmail.com>
2022-08-15 16:22:59 -0400

    test: add projectId to e2e object in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import { plugin as cypressFirebasePlugin } from 'cypress-firebase'
 
 const cypressConfig = defineConfig({
   e2e: {
+    projectId: 'ttvr1j',
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e/index.js',
     setupNodeEvents(on, config) {
