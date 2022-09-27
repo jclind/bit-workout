7e15513c34a084efec1156c4ea2ad81057837c27
Jesse Lind <Jesseclind@gmail.com>
2022-09-27 15:46:56 -0400

    style!: add scroll overflow to workout path modal

--- src/components/Workout/WorkoutPathModal/WorkoutPathModal.js
+++ src/components/Workout/WorkoutPathModal/WorkoutPathModal.js
@@ -115,145 +115,152 @@ const WorkoutPathModal = ({
     <>
       <div className='workout-path-modal overlay'>
         <div className='modal-content' ref={modalContent}>
-          <div className='settings-title'>Workout Path</div>
-          {error && (
-            <div className='form-error'>
-              <AiOutlineWarning className='icon' />
-              {error}
-            </div>
-          )}
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId='droppable'>
-              {(provided, snapshot) => (
-                <div
-                  className='path'
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {currWorkoutPath.map((ex, idx) => {
-                    const currExercise = exerciseList.find(
-                      exercise => exercise.id === ex.exerciseID
-                    )
-                    const imageURL = currExercise.imageURL
-                    const name = currExercise.name
-                    const sets = ex.sets
-                    const setType = ex.type
-                    const numSets = sets.length
-                    let currExerciseWeight = null
-                    if (currExercise.weights) {
-                      if (setType === 'straight') {
-                        let weightObj = weights.find(
-                          weight => weight.exerciseID === ex.exerciseID
-                        )
-                        currExerciseWeight =
-                          weightObj && weightObj.weight ? weightObj.weight : 45
-                      } else if (setType === 'drop' || setType === 'timed') {
-                        const maxWeight = Math.max.apply(
-                          Math,
-                          sets.map(o => Number(o.weight))
-                        )
-                        const minWeight = Math.min.apply(
-                          Math,
-                          sets.map(o => Number(o.weight))
-                        )
+          <div className='container'>
+            <div className='settings-title'>Workout Path</div>
+            {error && (
+              <div className='form-error'>
+                <AiOutlineWarning className='icon' />
+                {error}
+              </div>
+            )}
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId='droppable'>
+                {(provided, snapshot) => (
+                  <div
+                    className='path'
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {currWorkoutPath.map((ex, idx) => {
+                      const currExercise = exerciseList.find(
+                        exercise => exercise.id === ex.exerciseID
+                      )
+                      const imageURL = currExercise.imageURL
+                      const name = currExercise.name
+                      const sets = ex.sets
+                      const setType = ex.type
+                      const numSets = sets.length
+                      let currExerciseWeight = null
+                      if (currExercise.weights) {
+                        if (setType === 'straight') {
+                          let weightObj = weights.find(
+                            weight => weight.exerciseID === ex.exerciseID
+                          )
+                          currExerciseWeight =
+                            weightObj && weightObj.weight
+                              ? weightObj.weight
+                              : 45
+                        } else if (setType === 'drop' || setType === 'timed') {
+                          const maxWeight = Math.max.apply(
+                            Math,
+                            sets.map(o => Number(o.weight))
+                          )
+                          const minWeight = Math.min.apply(
+                            Math,
+                            sets.map(o => Number(o.weight))
+                          )
 
-                        currExerciseWeight =
-                          maxWeight + (minWeight < maxWeight && `-${minWeight}`)
-                      }
-                    }
-
-                    const exerciseState =
-                      currExerciseIdx > idx
-                        ? 'COMPLETED'
-                        : currExerciseIdx === idx
-                        ? 'ACTIVE'
-                        : 'UPCOMING'
-
-                    // If exercise has been completed, completed sets will equal total sets
-                    // If exercise is active, completed sets will equal the current workout set minus one (to get number of completed sets)
-                    // If exercise is upcoming, 0 sets will have been completed
-                    let completedSets = 0
-                    if (exerciseState === 'COMPLETED') completedSets = numSets
-                    if (exerciseState === 'ACTIVE')
-                      completedSets = currSetIdx - 1
-                    return (
-                      <Draggable
-                        key={idx}
-                        draggableId={`draggable-${idx}`}
-                        index={idx}
-                        isDragDisabled={
-                          exerciseState === 'COMPLETED' || !isEditing
+                          currExerciseWeight =
+                            maxWeight +
+                            (minWeight < maxWeight && `-${minWeight}`)
                         }
-                      >
-                        {(provided, snapshot) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            className={`workout-path-exercise ${exerciseState}${
-                              isEditing ? ' editing' : ''
-                            }`}
-                          >
+                      }
+
+                      const exerciseState =
+                        currExerciseIdx > idx
+                          ? 'COMPLETED'
+                          : currExerciseIdx === idx
+                          ? 'ACTIVE'
+                          : 'UPCOMING'
+
+                      // If exercise has been completed, completed sets will equal total sets
+                      // If exercise is active, completed sets will equal the current workout set minus one (to get number of completed sets)
+                      // If exercise is upcoming, 0 sets will have been completed
+                      let completedSets = 0
+                      if (exerciseState === 'COMPLETED') completedSets = numSets
+                      if (exerciseState === 'ACTIVE')
+                        completedSets = currSetIdx - 1
+                      return (
+                        <Draggable
+                          key={idx}
+                          draggableId={`draggable-${idx}`}
+                          index={idx}
+                          isDragDisabled={
+                            exerciseState === 'COMPLETED' || !isEditing
+                          }
+                        >
+                          {(provided, snapshot) => (
                             <div
-                              className={`container${
-                                isEditing && exerciseState !== 'COMPLETED'
-                                  ? ' jiggle'
-                                  : ''
-                              }${snapshot.isDragging ? ' dragging' : ''}`}
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className={`workout-path-exercise ${exerciseState}${
+                                isEditing ? ' editing' : ''
+                              }`}
                             >
-                              <div className='image'>
-                                <img src={imageURL} alt={name} />
-                              </div>
-                              <div className='exercise-data'>
-                                <div className='name'>{name}</div>
-                                <div className='reps-sets'>
-                                  <div className='sets'>
-                                    {setType} Sets:{' '}
-                                    <span>
-                                      <span className='curr-set'>
-                                        {completedSets}
+                              <div
+                                className={`exercise-container${
+                                  isEditing && exerciseState !== 'COMPLETED'
+                                    ? ' jiggle'
+                                    : ''
+                                }${snapshot.isDragging ? ' dragging' : ''}`}
+                              >
+                                <div className='image'>
+                                  <img src={imageURL} alt={name} />
+                                </div>
+                                <div className='exercise-data'>
+                                  <div className='name'>{name}</div>
+                                  <div className='reps-sets'>
+                                    <div className='sets'>
+                                      {setType} Sets:{' '}
+                                      <span>
+                                        <span className='curr-set'>
+                                          {completedSets}
+                                        </span>
+                                        /{numSets}
                                       </span>
-                                      /{numSets}
-                                    </span>
+                                    </div>
                                   </div>
                                 </div>
+                                {currExerciseWeight && (
+                                  <div className='weight'>
+                                    {currExerciseWeight}lbs
+                                  </div>
+                                )}
                               </div>
-                              {currExerciseWeight && (
-                                <div className='weight'>
-                                  {currExerciseWeight}lbs
-                                </div>
-                              )}
+                              {workout.path.length > 1 && isEditing ? (
+                                <button
+                                  className='remove-exercise-btn'
+                                  onClick={() =>
+                                    handleConfirmRemoveExercise(idx)
+                                  }
+                                >
+                                  <AiOutlineClose className='icon' />
+                                </button>
+                              ) : null}
                             </div>
-                            {workout.path.length > 1 && isEditing ? (
-                              <button
-                                className='remove-exercise-btn'
-                                onClick={() => handleConfirmRemoveExercise(idx)}
-                              >
-                                <AiOutlineClose className='icon' />
-                              </button>
-                            ) : null}
-                          </div>
-                        )}
-                      </Draggable>
-                    )
-                  })}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
-          <button
-            className='editing-btn'
-            onClick={() => setIsEditing(prev => !prev)}
-          >
-            {isEditing ? 'Done' : 'Edit'}
-          </button>
-          <button
-            className='add-exercise'
-            onClick={() => setIsExerciseToWorkoutModalOpen(true)}
-          >
-            <AiOutlinePlusCircle className='icon' /> <span>Add Exercise</span>
-          </button>
+                          )}
+                        </Draggable>
+                      )
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+            <button
+              className='editing-btn'
+              onClick={() => setIsEditing(prev => !prev)}
+            >
+              {isEditing ? 'Done' : 'Edit'}
+            </button>
+            <button
+              className='add-exercise'
+              onClick={() => setIsExerciseToWorkoutModalOpen(true)}
+            >
+              <AiOutlinePlusCircle className='icon' /> <span>Add Exercise</span>
+            </button>
+          </div>
         </div>
       </div>
       {isExerciseToWorkoutModalOpen ? (
