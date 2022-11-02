1b458720f9c13d7c758c2e035340df724f00871f
Jesse Lind <Jesseclind@gmail.com>
2022-11-02 15:01:27 -0400

    fix: only show splash screen when user is logged in

--- src/components/NewsSplashScreen/NewsSplashScreen.js
+++ src/components/NewsSplashScreen/NewsSplashScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import ReactDom from 'react-dom'
+import { connect } from 'react-redux'
 import useClickOutside from '../../util/useClickOutside'
 
-const NewsSplashScreen = () => {
+const NewsSplashScreen = ({ isSignedIn }) => {
   const [isOpen, setIsOpen] = useState(false)
 
   const currSplashScreenKey = 'd3dce5cf-10ab-4d3a-ae96-e405dda86752'
@@ -12,7 +13,7 @@ const NewsSplashScreen = () => {
 
   useEffect(() => {
     const localStorageKey = localStorage.getItem('splashScreenKey')
-    if (localStorageKey === currSplashScreenKey) {
+    if (localStorageKey === currSplashScreenKey || !isSignedIn) {
       setIsOpen(false)
     } else {
       localStorage.setItem('splashScreenKey', currSplashScreenKey)
@@ -34,4 +35,10 @@ const NewsSplashScreen = () => {
   )
 }
 
-export default NewsSplashScreen
+const mapStateToProps = state => {
+  return {
+    isSignedIn: !!state?.auth?.userAuth?.uid,
+  }
+}
+
+export default connect(mapStateToProps)(NewsSplashScreen)
